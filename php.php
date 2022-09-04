a253f0060e5f9eb30fb1782e1ee698b74d187a29
Sara <a20sarle@student.his.se>
2022-09-04 14:13:23 +0200

    🟢 Bootstrap - one modal for each card - php

--- php.php
+++ php.php
@@ -7,8 +7,10 @@
   <title>Document</title>
   <!-- bootstrap only css -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-  <!-- bootstrap JavaScript Bundle with Popper -->
+  <!-- bootstrap JavaScript Bundle with Popper for masonry -->
   <script src="https://cdn.jsdelivr.net/npm/masonry-layout@4.2.2/dist/masonry.pkgd.min.js" integrity="sha384-GNFwBvfVxBkLMJpYMOABq3c+d3KnQxudP/mGPkzpZSTYykLBNsZEnG2D9G/X/+7D" crossorigin="anonymous" async></script>
+  <!-- JavaScript Bundle with Popper for modal -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   <style>
     .bd-placeholder-img {
       font-size: 1.125rem;
@@ -30,11 +32,12 @@
   <?php
     $divCardColumnBreakpoints = "col-sm-6 col-lg-4 mb-4";
     $blockquoteFooterProperties = "blockquote-footer mb-0 text-muted";
+    $timesToPrintUsingLoop = 4;
   ?>
 
- <main class="container py-5">
-   <div class="row" data-masonry='{"percentPosition": true }'>
-      <?php for($i = 0; $i < 4; $i++) { ?>
+  <main class="container py-5">
+    <div class="row" data-masonry='{"percentPosition": true }'>
+      <?php for($i = 0; $i < $timesToPrintUsingLoop; $i++) { ?>
         <div class="<?php echo $divCardColumnBreakpoints ?>">
           <div class="card">
             <svg class="bd-placeholder-img card-img-top" width="100%" height="200" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" focusable="false">
@@ -45,12 +48,37 @@
             <div class="card-body">
               <h5 class="card-title">Card title that wraps to a new line</h5>
               <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <!-- Button triggering modal to open -->
+              <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal<?php echo $i?>">
+                Launch demo modal <?php echo $i?>
+              </button>
             </div>
           </div>
-        </div>
+        </div>        
       <?php } ?> 
     </div>
   </main>
+
+  <?php for($i = 0; $i < $timesToPrintUsingLoop; $i++) { ?>
+    <!-- Modal - must be places outside any container to be positioned correctly on the screen on launch -->
+    <div class="modal fade" id="exampleModal<?php echo $i?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Modal title <?php echo $i?></h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            ...
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  <?php } ?>
   
 </body>
 </html>
